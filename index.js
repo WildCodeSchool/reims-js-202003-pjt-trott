a5a9f5bd3dee7e9c2563714a21b5a499c2925aef
Victorafx <victor.auxietrefontes@laposte.net>
2020-06-03 11:46:28 +0200

    refactor: fix indentation in indexjs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 app.listen(port, (err) => {
     if (err) {
-      throw new Error('Something bad happened...');
-    } 
+        throw new Error('Something bad happened...');
+    }
     console.log(`Server is listening on ${port}`);
-  });
+});
