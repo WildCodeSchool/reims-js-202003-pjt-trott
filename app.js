7ba951b7fe3678af7dd14c09f3500cc3dc89cc46
Brian <brian.lagaude@gmail.com>
2020-06-16 16:46:50 +0200

    refactor: connect routes to mysql

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const api = require('./routes');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use('/api', api)
+
+app.get('/', (request,response) => {
+  response.send({message: `Hello World`})
+})
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const port = 8000;
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
 
 app.listen(port, (err) => {
     if (err) {
-        throw new Error('Something bad happened...');
+        throw new Error(`An error occured: ${err.message}`);
     }
     console.log(`Server is listening on ${port}`);
 });
