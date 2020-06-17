8806d98c58ecd7ab038d90b00de4223530e801bb
BrianLag <61591219+BrianLag@users.noreply.github.com>
2020-06-17 16:30:52 +0200

    Delete conf.js

--- conf.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // adresse du serveur
-user :  'root', // le nom d'utilisateur
-password :  '', // le mot de passe
-database :  'trottTest', // le nom de la base de données
-});
-module.exports = connection;
