e5f28a770b2fc0ad556e8cdfb748f8afbb0566e2
Anthony Lucci <totowdu80@gmail.com>
2020-07-01 14:29:24 +0200

    refactor: update useless and not good code after PR request

--- routes/register.js
+++ routes/register.js
@@ -2,42 +2,13 @@ const express = require('express');
 const connection = require('../conf');
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-  connection.query('SELECT * from register', (err, results) => {
-    if(err) {
-      return(
-        res.status(500).send(`We didn't find users `)
-      )
-    }
-    res.json(results)
-  })
-})
-
-router.get('/:id', (req, res) => {
-  const {id} = req.params
-  connection.query('SELECT * FROM register WHERE ID = ?', id, (err, results) => {
-    if(err) {
-      return(
-        res.status(500).send('Internal server error')
-      )
-    }
-    if (results.length === 0) {
-      return(
-        res.status(404).send('userId is not found')
-      )
-    }
-    res.json(results)
-  });
-});
-
 router.post('/', (req, res) => {
   const postUser = req.body;
   console.log(`hello`,req.body);
   if (postUser.username == null || postUser.username == ''){
     res.status(400).send("le nom de l'utilisateur a été mal renseigné")
   }
-  connection.query('INSERT INTO register SET ?', postUser, (err, results) => {
+  connection.query('INSERT INTO user SET ?', postUser, (err, results) => {
     if (err) {
       return (
         res.status(500).send("Erreur lors de la sauvegarde d'un utilisateur")
