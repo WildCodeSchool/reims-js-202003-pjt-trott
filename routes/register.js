c71ce033837a4dd49e9955313ccd952bbf4b14d2
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-09 10:50:28 +0200

    refactor(register): add crypt password when the user register

--- routes/register.js
+++ routes/register.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const connection = require('../conf');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
   const postUser = req.body;
   console.log(`hello`,req.body);
   if (postUser.username == null || postUser.username == ''){
     res.status(400).send("le nom de l'utilisateur a été mal renseigné")
+  } else {
+    bcrypt.hash(postUser.password, 10, function(err, hash) {
+      postUser.password = hash
+      connection.query('INSERT INTO user SET ?', postUser, (err, results) => {
+        if (err) {
+          return (
+            res.status(500).send("Erreur lors de la sauvegarde d'un utilisateur")
+          )
+        } else {
+        res.status(201).json({ ...postUser, id: results.insertId })
+        }
+      });
+    });
   }
-  connection.query('INSERT INTO user SET ?', postUser, (err, results) => {
-    if (err) {
-      return (
-        res.status(500).send("Erreur lors de la sauvegarde d'un utilisateur")
-      )
-    }
-    res.status(201).json({ ...postUser, id: results.insertId })
-  });
 });
+  
 
 module.exports = router
